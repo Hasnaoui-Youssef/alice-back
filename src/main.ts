013eca3c9f7f01b3470d7fbda6f2e63ef871bb62
Hasnaoui-Youssef <hasneouiyoussef@gmail.com>
2025-02-16 14:20:47 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http_exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser.default());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
